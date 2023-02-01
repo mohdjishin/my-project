cf332ac75a138d62e47305380dfd35df0b4c4c1b
mohdjishin <whoistcp@protonmail.com>
2023-02-01 21:35:29 +0530

    sub

--- main.go
+++ main.go
@@ -10,3 +10,6 @@ func main() {
 func add(a, b int) int {
 	return a + b
 }
+func sub(a, b int) int {
+	return a - b
+}
--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_add(t *testing.T) {
 	type args struct {
@@ -18,6 +20,27 @@ func Test_add(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := add(tt.args.a, tt.args.b); got != tt.want {
 				t.Errorf("add() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_sub(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sub(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("sub() = %v, want %v", got, tt.want)
 			}
 		})
 	}
